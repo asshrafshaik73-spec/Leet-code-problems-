c4552beb825a2a319c15d3a17fc5a9254ffec461
asshrafshaik73-spec <asshrafshaik73@gmail.com>
2026-07-27 11:34:41 +0530

    Create 2618.Check if object instance of class.java

---
+++ 2618.Check if object instance of class.java
@@ -0,0 +1,14 @@
+/**
+ * @param {*} value
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(value, classFunction) {
+    if (value === null || value === undefined) return false;
+    if (typeof classFunction !== "function") return false;
+
+    return (
+        value instanceof classFunction ||
+        Object(value) instanceof classFunction
+    );
+};
